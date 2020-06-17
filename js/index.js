a593537a2dbec6480c917b06904a912dc485ef14
Sebastian <Ferreira>
2020-06-16 23:32:56 -0300

    add function setStoresList, CSS for the list

--- js/index.js
+++ js/index.js
@@ -27,41 +27,63 @@ const createMarker = (storeLat, storeLng, name, index) => {
         title: name,
         label: `${index}`
     });
-    google.maps.event.addListener(marker, 'click', function() {
+    google.maps.event.addListener(marker, 'click', function () {
         infoWindow.setContent(html);
         infoWindow.open(map, marker);
-      });
-}
-
-const getStores=()=>{
-    const API_URL = 'http://localhost:3000/api/stores';
-    fetch(API_URL)
-    .then((response)=>{
-        if(response.status == 200){
-            return response.json();
-        } else {
-            throw new Error(response.status);
-        }
-
-    }).then((data)=>{
-        //console.log(data);
-        searchLocationsNear(data);
     });
 }
 
-const searchLocationsNear=(stores)=>{
-    stores.forEach((store, index)=>{
-        //console.log(store.name);
-            let storeLat = store.latitude;
-            let storeLng = store.longitude;
-            let storeName = store.name;
-            let titleName = '';
-            for(let i of storeName){
-                //console.log(i);
-                if(i=='<'){ break; }
-                titleName+=i;
-
+const getStores = () => {
+    const API_URL = 'http://localhost:3000/api/stores';
+    fetch(API_URL)
+        .then((response) => {
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                throw new Error(response.status);
             }
-        createMarker(storeLat, storeLng, titleName, index+1);
+
+        }).then((data) => {
+            //console.log(data);
+            searchLocationsNear(data);
+            setStoresList(data);
+
+        });
+}
+
+const searchLocationsNear = (stores) => {
+    stores.forEach((store, index) => {
+        //console.log(store.name);
+        let storeLat = store.latitude;
+        let storeLng = store.longitude;
+        let storeName = store.name;
+        let titleName = changeName(storeName);
+
+        createMarker(storeLat, storeLng, titleName, index + 1);
+
     })
+}
+const changeName = (title) => {
+    let titleName = '';
+    for (let i of title) {
+        //console.log(i);
+        if (i == '<') {
+            break;
+        }
+        titleName += i;
+    }
+    return titleName;
+}
+const setStoresList = (stores) => {
+    let htmlDiv = '';
+    stores.forEach((store, index) => {
+        htmlDiv +=`<div class="background">
+                    <div class="store">
+                    <div class="store-title">${store.name}</div>
+                    <div class="store-index">${index+1}</div>
+                    </div>
+                    </div>`;
+    })
+
+    document.querySelector('.store-info-container').innerHTML = htmlDiv;
 }
